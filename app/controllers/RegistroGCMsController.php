81cf2832fe6881e95676968a16d69cbbd3da0334
Andres Vasquez <andres.vasquez.a@hotmail.com>
2015-11-04 20:01:55 -0400

    Fipaz 1000 notificaciones

--- app/controllers/RegistroGCMsController.php
+++ app/controllers/RegistroGCMsController.php
@@ -122,7 +122,7 @@ class RegistroGCMsController extends \BaseController {
         $sistemas= SistemasDesarrollados::whereRaw('app=?',array($data["credencial"]))->get();
         if(sizeof($sistemas)>0) {
             $id_sistema = $sistemas[0]["id"];
-            $gcmUsers= RegistroGCM::whereRaw('sistema_id=? AND estado=1 AND baja_logica=1',array($id_sistema))->get();
+            $gcmUsers= RegistroGCM::whereRaw('sistema_id=? AND estado=1 AND baja_logica=1 ORDER BY RAND() LIMIT 999',array($id_sistema))->get();
             if(sizeof($gcmUsers)>0)
             {
                 $resultado=array();
