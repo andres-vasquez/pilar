6f2253e0cad769770e6cccf72242d82693b1f4f4
Andres Vasquez <andres.vasquez.a@hotmail.com>
2015-12-14 17:16:17 -0400

    Publicacion con Obj, fixed fecha_publicacion

--- app/models/DrClippingPublicacion.php
+++ app/models/DrClippingPublicacion.php
@@ -17,6 +17,6 @@ class DrClippingPublicacion extends \Eloquent {
 	
 	// Campos a incluirse en insert y update
 	protected $fillable = array('aud_usuario_id','aud_usuario_mod_id','estado','baja_logica','usuario_id','ciudad_id',
-        'ciudad','tipo_medio_id','tipo_medio','medio_id','medio','fecha_publiccion','ubicacion_id','ubicacion',
+        'ciudad','tipo_medio_id','tipo_medio','medio_id','medio','fecha_publicacion','ubicacion_id','ubicacion',
         'pagina','empresa','tipo_noticia_id','tipo_noticia','url_foto1','url_foto2','estado_tarea');
 }
