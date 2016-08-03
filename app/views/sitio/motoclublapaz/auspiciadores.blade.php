0c6a2dd147a301960b0ad61ad4c0827325631c5c
Andres Vasquez <andres.vasquez.a@hotmail.com>
2016-08-03 00:01:02 -0400

    Motoclub La Paz Auspiciadores segundo

--- app/views/sitio/motoclublapaz/auspiciadores.blade.php
+++ app/views/sitio/motoclublapaz/auspiciadores.blade.php
@@ -31,18 +31,241 @@
             <br/>
 
             <div class="row">
-                <div class="col-xs-6 col-md-6 col-lg-3">
+                <div class="col-xs-4 col-md-6 col-lg-3">
                     <img src="<% URL::asset('public/img/auspiciadores/TaylorRiders.png');%>" class="img-responsive"/>
                 </div>
-                <div class="col-xs-6 col-md-6 col-lg-3">
+                <div class="col-xs-8 col-md-6 col-lg-3">
                     <strong>TAILOR Riders</strong><br/>
                     <b>Contacto:</b> Miguel Ángel Rocha<br/>
                     Cel : 76566658
                 </div>
             </div>
+            <br/>
+            <div class="row">
+                <div class="col-xs-4 col-md-6 col-lg-3">
+                    <img src="<% URL::asset('public/img/auspiciadores/DMAndino.png');%>" class="img-responsive"/>
+                </div>
+                <div class="col-xs-8 col-md-6 col-lg-3">
+                    <strong>DM Andino</strong><br/>
+                    <b>Contacto:</b> Gabriela Monje<br/>
+                    Cel: 76747797<br/>
+                    Pagina web: www.dmhoteles.com
+                </div>
+            </div>
+            <br/>
+            <div class="row">
 
-            <br/><br/>
-            <a href="http://jakare.net">Jakare SRL.</a>
+                <div class="col-xs-4 col-md-6 col-lg-3">
+                    <img src="<% URL::asset('public/img/auspiciadores/kaisha.png');%>" class="img-responsive"/>
+                </div>
+                <div class="col-xs-8 col-md-6 col-lg-3">
+                    <strong>Kaisha Corporation SRL.</strong><br/>
+                    <b>Contacto:</b> Lic. Juan Carlos Vacaflor, MBA<br/>
+                    Celular: 767 76703
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-xs-4 col-md-6 col-lg-3">
+                    <img src="<% URL::asset('public/img/auspiciadores/AraujoyForgues.png');%>" class="img-responsive"/>
+                </div>
+                <div class="col-xs-8 col-md-6 col-lg-3">
+                    <strong>Araujo &amp; Forgues SC</strong><br/>
+                    <b>Contacto:</b> Dra. Jeannine Forguez<br/>
+                    Email: jforgues@auaujoforgues.com
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-xs-4 col-md-6 col-lg-3">
+                    <img src="<% URL::asset('public/img/auspiciadores/4K.png');%>" class="img-responsive"/>
+                </div>
+                <div class="col-xs-8 col-md-6 col-lg-3">
+                    <strong>4K - Imtex</strong><br/>
+                    <b>Contacto:</b> Mauricio del Solar<br/>
+                    Cel: (7) 678-5531<br/>
+                    Página web: http://www.imtex.com.bo
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-xs-4 col-md-6 col-lg-3">
+                    <img src="<% URL::asset('public/img/auspiciadores/LiquiMoly.png');%>" class="img-responsive"/>
+                </div>
+                <div class="col-xs-8 col-md-6 col-lg-3">
+                    <strong>Liqui Moly</strong><br/>
+                    <b>Contacto:</b> Domenico Pirazolli<br/>
+                    Celular: 591 (7) 634-2777
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-xs-4 col-md-6 col-lg-3">
+                    <img src="<% URL::asset('public/img/auspiciadores/Imobilare_Salinas.png');%>" class="img-responsive"/>
+                </div>
+                <div class="col-xs-8 col-md-6 col-lg-3">
+                    <strong>Inmobiliare & Salinas</strong><br/>
+                    <b>Contacto:</b> Ing. Pablo Salinas
+                    Celular: (7) 723-7406
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-xs-4 col-md-6 col-lg-3">
+                    <img src="<% URL::asset('public/img/auspiciadores/Amaszonas.png');%>" class="img-responsive"/>
+                </div>
+                <div class="col-xs-8 col-md-6 col-lg-3">
+                    <strong>Línea Aérea Amaszonas S.A.</strong><br/>
+                    <b>Contacto:</b> Lic. Claudia Cadena Aranda<br/>
+                    SUPERVISOR DE VENTAS LPB 591 2 220848 Int. 2702 I 591 71133291<br/>
+                    Email: ccadena@amaszonas.com<br/>
+                    Dirección: Av. Hernando Siles, esq. Calle 10 s/n. Edificio Torre Obrajes, Piso 9<br/>
+                    Página web: www.amaszonas.com<br/>
+                    La Paz – Bolivia
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-xs-4 col-md-6 col-lg-3">
+                    <img src="<% URL::asset('public/img/auspiciadores/TransGTI.png');%>" class="img-responsive"/>
+                </div>
+                <div class="col-xs-8 col-md-6 col-lg-3">
+                    <strong>Trans Gti</strong><br/>
+                    <b>Contacto:</b> Nelson Aguirre Beckard<br/>
+                    Email: Nelo33415@hotmail.com<br/>
+                    Cel: 77705695 – 7856253
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-xs-4 col-md-6 col-lg-3">
+                    <img src="<% URL::asset('public/img/auspiciadores/LaBolivianaCiacruz.png');%>" class="img-responsive"/>
+                </div>
+                <div class="col-xs-8 col-md-6 col-lg-3">
+                    <strong>La Boliviana Ciacruz</strong><br/>
+                    <b>Contacto:</b> Carmen Sánchez Molina<br/>
+                    Subgerente Nacional Comunicaciones<br/>
+                    La Boliviana Ciacruz Seguros<br/>
+                    Celular: (591) 72028611<br/>
+                    Número piloto (591) 2-2203131 int 7860<br/>
+                    Email: carmen.sanchez@lbc.bo<br/>
+                    Página web: www.lbc.bo
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-xs-4 col-md-6 col-lg-3">
+                    <img src="<% URL::asset('public/img/auspiciadores/tigo.png');%>" class="img-responsive"/>
+                </div>
+                <div class="col-xs-8 col-md-6 col-lg-3">
+                    <strong>Tigo</strong><br/>
+                    <b>Contacto:</b> L. Gabriel Archondo Quintela<br/>
+                    Territory Marketing Coordinator North<br/>
+                    Marketing<br/>
+                    Celular: (591)77290013 Interno: (591)77250524<br/>
+                    Dirección: Av. Marisca Santa Cruz esq. Yanacocha edif. Hansa<br/>
+                    La Paz, Bolivia<br/>
+                    Página web: www.tigo.com.bo
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-xs-4 col-md-6 col-lg-3">
+                    <img src="<% URL::asset('public/img/auspiciadores/DYM.png');%>" class="img-responsive"/>
+                </div>
+                <div class="col-xs-8 col-md-6 col-lg-3">
+                    <strong>DISTRIBUCION Y MERCADEO LTDA. (D&amp;M LTDA.)</strong><br/>
+                    Dirección: Calle Estados Unidos N° 1427 - Miraflores<br/>
+                    Teléfono: (591) (2) 222 5961<br/>
+                    Fax: (591) (3) 222 5485<br/>
+                    E-mail: jtrigo@dym-bo.com
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-xs-4 col-md-6 col-lg-3">
+                    <img src="<% URL::asset('public/img/auspiciadores/Paceña.png');%>" class="img-responsive"/>
+                </div>
+                <div class="col-xs-8 col-md-6 col-lg-3">
+                    <strong>Cerveceria Boliviana Nacional S.A</strong><br/>
+                    Dirección :Av. Montes # 400<br/>
+                    Telf: (591-2) 2455455 / 2454454<br/>
+                    Fax: (591-2) 2455344<br/>
+                    Casilla 421<br/>
+                    Página web: https://www.cbn.com.bo
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-xs-4 col-md-6 col-lg-3">
+                    <img src="<% URL::asset('public/img/auspiciadores/EBOTV.png');%>" class="img-responsive"/>
+                </div>
+                <div class="col-xs-8 col-md-6 col-lg-3">
+                    <strong>Empresa Boliviana de Television</strong><br/>
+                    Contacto: Lic. Jorge Inchausti<br/>
+                    Telf: 591 (7) 229-1070<br/>
+                    Página web: http://www.ebo.tv
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-xs-4 col-md-6 col-lg-3">
+                    <img src="<% URL::asset('public/img/auspiciadores/Factory.png');%>" class="img-responsive"/>
+                </div>
+                <div class="col-xs-8 col-md-6 col-lg-3">
+                    <strong>Factory</strong><br/>
+                    Dirección: Complejo Megacenter, Irpavi.<br/>
+                    Telefono: 214 8306<br/>
+                    Email: administracion@factory.com.bo<br/>
+                    Página web: www.factory.com.bo
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-xs-4 col-md-6 col-lg-3">
+                    <img src="<% URL::asset('public/img/auspiciadores/OCIS.png');%>" class="img-responsive"/>
+                </div>
+                <div class="col-xs-8 col-md-6 col-lg-3">
+                    <strong>OCIS</strong><br/>
+                    Dirección: Av. 6 de Agosto Nº 2700<br/>
+                    Edif. Torre Empresarial CADECO<br/>
+                    La Paz - Bolivia<br/>
+                    Teléfono.: 591-2 2912462<br/>
+                    Fax.: 591-2 2117905<br/>
+                    Email: ocis@obrascivilesindustriales.com
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-xs-4 col-md-6 col-lg-3">
+                    <img src="<% URL::asset('public/img/auspiciadores/Nosiglia1.png');%>" class="img-responsive"/>
+                    <br/>
+                    <img src="<% URL::asset('public/img/auspiciadores/Nosiglia2.png');%>" class="img-responsive"/>
+                </div>
+                <div class="col-xs-8 col-md-6 col-lg-3">
+                    <strong>Race store</strong><br/>
+                    Dirección: Av. Ballivian esq. calle 19 Calacoto<br/>
+                    Teléfono: 2771088<br/>
+                    Página web: www.racebolivia.com<br/>
+                    <br/>
+                    <strong>Nosiglia sport</strong><br/>
+                    Dirección: Av Costanera #29<br/>
+                    Teléfono: 2794904
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-xs-4 col-md-6 col-lg-3">
+                    <img src="<% URL::asset('public/img/auspiciadores/jakare.png');%>" class="img-responsive"/>
+                </div>
+                <div class="col-xs-8 col-md-6 col-lg-3">
+                    <strong>Jakare Srl.</strong><br/>
+                    Teléfono: +591 78812086<br/>
+                    Email: info@jakare.net<br/>
+                    <a href="http://jakare.net">Jakare SRL.</a>
+                </div>
+            </div>
+
             <br/><br/>
         </div>
     </div>
